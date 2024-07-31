aab2e1d145cdd0303e29914f8d5ca77c3a388e5b
Dipanshu9630 <Deepanshupandey30@gmail.com>
2024-07-31 14:31:46 +0530

    Email service

---
+++ src/main/java/dp/practice/project/Book_App_Project/Services/EmailService.java
@@ -0,0 +1,22 @@
+package dp.practice.project.Book_App_Project.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    public void sendEmail(String to, String subject, String body){
+        SimpleMailMessage mailMessage=new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(body);
+        mailMessage.setFrom("dipanshu.p@outlook.com");
+        javaMailSender.send(mailMessage);
+
+    }
+}
