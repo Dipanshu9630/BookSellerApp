37f67d074e16be444d0a8fa66c023af00d849582
Dipanshu9630 <Deepanshupandey30@gmail.com>
2024-07-31 14:32:23 +0530

    repository interface

---
+++ src/main/java/dp/practice/project/Book_App_Project/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package dp.practice.project.Book_App_Project.repositories;
+
+import dp.practice.project.Book_App_Project.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+    Optional<Object> findByEmail(String email);
+}
